c98c2203fd1623d5c312de927d1c0a06a9f3090b
BVASQUEZ\bvasq <bvasquez>
2026-08-24 17:39:44 -0500

    quitar el periodo de la secuencia

--- database/db_store_01_mysql/procedures/get_cod_trx.sql
+++ database/db_store_01_mysql/procedures/get_cod_trx.sql
@@ -1,5 +1,8 @@
-DELIMITER ;;
-CREATE PROCEDURE `db_store_01`.`get_cod_trx`(
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `get_cod_trx` $$
+
+CREATE PROCEDURE `get_cod_trx`(
 	IN p_storeCod VARCHAR(4),
 	IN p_SequenceTableType VARCHAR(32)
 )
@@ -14,7 +17,7 @@ begin
 	and SequenceTableType = p_SequenceTableType
 	and PeriodId in (select P.PeriodId from period P where P.Status = 'A');
 
-	select CONCAT(Prefix,StoreCod,LPAD(PeriodId,4,0),LPAD(SequenceTrx, SequenceLength, '0')) into l_cod_trx
+	select CONCAT(Prefix,StoreCod,LPAD(SequenceTrx, SequenceLength, '0')) into l_cod_trx
 	from store_sequence 
 	where StoreCod = p_StoreCod
 	and SequenceTableType = p_SequenceTableType
@@ -22,5 +25,5 @@ begin
 
 	select l_cod_trx as cod_trx;
 
-END ;;
+END $$
 DELIMITER ;
